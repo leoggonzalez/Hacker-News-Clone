805fea88199c161210586fe0903e5ac326f17c95
leocirculo <lasociedadloka@gmail.com>
2018-10-02 19:31:16 +0200

    corrected webpack scripts for scss

--- scripts/start.js
+++ scripts/start.js
@@ -79,10 +79,7 @@ choosePort(HOST, DEFAULT_PORT)
       proxyConfig,
       urls.lanUrlForConfig
     );
-    const devServer = new WebpackDevServer(compiler, {
-      ...serverConfig,
-      https: true
-    });
+    const devServer = new WebpackDevServer(compiler, serverConfig);
     // Launch WebpackDevServer.
     devServer.listen(port, HOST, err => {
       if (err) {
